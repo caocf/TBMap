0738b7033e0e8f267f898403c3cbde9183e76aeb
libo_liu <qq644531343@gmail.com>
2014-11-05 11:00:54 +0800

    工程添加

---
+++ TestTBMap/TestTBMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTBMap
+//
+//  Created by libo on 11/5/14.
+//  Copyright (c) 2014 sina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTBMap/TestTBMap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestTBMap
+//
+//  Created by libo on 11/5/14.
+//  Copyright (c) 2014 sina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
